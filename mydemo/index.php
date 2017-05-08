eb678330fdd071a96bd11cc3baf2bfc77c1f28cd
Big-stone <597685607@qq.com>
2017-05-08 17:24:54 +0800

    初步实现登录功能-未完成

---
+++ mydemo/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+    // print_r($_SERVER)
+    $path = 'index';
+    $filename = 'index';
+    // 处理请求路径
+    if(array_key_exists('PATH_INFO',$_SERVER)){
+        $url = $_SERVER['PATH_INFO'];
+        $arr = explode('/',substr($url,1));
+        if(count($arr) == 2){
+            $path = $arr[0];//路径
+            $filename = $arr[1];//文件名称
+        }else{
+            $filename = 'login';
+        }
+    }else{
+        $filename = 'login';
+    }
+    //载入页面
+    include('./view/'.$path.'/'.$filename.'.html');
+?>
---
+++ mydemo/public/js/common.js
@@ -0,0 +1,27 @@
+// 控制左侧导航菜单的折叠
+	$('.navs ul').prev('a').on('click', function () {
+		$(this).next().slideToggle();
+	});
+
+	$('#loginForm').submit(function(){
+		//获取表单数据
+		var formData = $(this).serialize();
+		//调用接口
+		$.ajax({
+			type : 'post',
+			url : '/api/login',
+			data : formData,
+			dataType : 'json',
+			success : function(data){
+				// var tc_avatar = data.resault.tc_avatar;
+				// var tc_name = data.resault.tc_name;
+				//cookie可以实现页面间的数据共享
+				if(data.code == 200) {
+				location.href = '/index/index';
+				}
+			}
+		})
+		//jquery中的return false既可以阻止默认行为,也可以阻止冒泡
+		//原生js中的return false只能阻止默认行为，不能阻止冒泡
+		return false;
+	})
